b9970a67a8a5886594ac252e5228c9d0c09e7ef2
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-09-15 19:37:31 +0600

    Product description,related product

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum reiciendis, adipisci praesentium expedita aliquam nostrum nesciunt! Aut, architecto vero libero nam vel delectus dolor tempore obcaecati hic labore, corrupti illum eveniet nulla sequi? Ab placeat mollitia aut minus, rerum eveniet ullam reiciendis suscipit maiores, eligendi ad repellendus animi sint dolores corrupti ipsa adipisci laudantium fugiat?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere odit tenetur quos expedita asperiores suscipit, pariatur ipsa saepe culpa assumenda ut amet officiis tempora omnis animi quia cum, necessitatibus nihil.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
