fbf5f3e6e94047b828ab1db795776bc69333fdc8
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-10-28 01:08:03 +0600

    login, shopcategory

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -5,7 +5,7 @@ const LoginSignup = () => {
   return (
     <div className='loginsignup'>
         <div className="loginsignup-container">
-          <h1>Sign Up</h1>
+          <h1><i class="fa-solid fa-house-lock"></i> Sign Up</h1>
           <div className="loginsignup-fields">
             <input type="text" placeholder='Your Name' />
             <input type="email" placeholder='Email Address' />
@@ -15,7 +15,7 @@ const LoginSignup = () => {
           <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
           <div className="loginsignup-agree">
             <input type="checkbox"name='' id='' />
-            <p>By continuing, i agree to the terms of use & privacy policy.</p>
+            <p >By continuing, i agree to the terms of use & privacy policy.</p>
           </div>
         </div>
     </div>
